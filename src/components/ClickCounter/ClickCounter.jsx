edf90e3c03cac4b5d8334050c74ee59fec368c2e
Yehor Pliasov <fenix4088@gmail.com>
2021-10-06 19:45:54 +0300

    ClickCounter covered by tests

--- src/components/ClickCounter/ClickCounter.jsx
+++ src/components/ClickCounter/ClickCounter.jsx
@@ -1,20 +1,38 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 export const ClickCounter = () => {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
+    const [error, setError] = useState(false);
 
-  return (
-    <div data-test="component-app" className="App">
-      <h1 data-test="counter-display">
-        The counter is currently&nbsp;
-        <span data-test="count">{count}</span>
-      </h1>
-      <button
-        data-test="increment-button"
-        onClick={() => setCount(count + 1)}
-      >
-        Increment counter
-      </button>
-    </div>
-  );
+    const inc = () => {
+        setCount(count + 1)
+        setError(false);
+
+    }
+    const dec = () => {
+        count > 0 && setCount(count - 1)
+        count === 0 && setError(true);
+    }
+
+    return (
+        <div data-test="component-app" className="App">
+            <h1 data-test="counter-display">
+                The counter is currently&nbsp;
+                <span data-test="count">{count}</span>
+            </h1>
+            <button
+                data-test="increment-button"
+                onClick={inc}
+            >
+                Increment counter
+            </button>
+            <button
+                data-test="decrement-button"
+                onClick={dec}
+            >
+                Decrement counter
+            </button>
+            {error && <div data-test={'error-message'} style={{color: 'red'}}>Error counter couldn't be less then 0</div>}
+        </div>
+    );
 };
--- src/components/ClickCounter/ClickCounter.test.jsx
+++ src/components/ClickCounter/ClickCounter.test.jsx
@@ -1,22 +1,87 @@
-import { configure, shallow } from 'enzyme';
+import {configure, shallow} from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { ClickCounter } from './ClickCounter';
+import {ClickCounter} from './ClickCounter';
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
+
+
+/*
+* Factory function to create shallow wrapper for the ClickCounter comp
+* @function setUp
+* @returns {ShallowWrapper}
+**/
+const setUp = () => shallow(<ClickCounter/>);
+
+const findByTestAttr = (wrapper, attr) => wrapper.find(`[data-test='${attr}']`);
 
 describe('ClickCounter', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<ClickCounter />);
-  });
+    let wrapper;
+    beforeEach(() => {
+        wrapper = setUp(<ClickCounter/>);
+    });
 
-  it('should renders without error', () => {
-    const appComponent = wrapper.find('[data-test="component-app"]');
+    it('should renders without error', () => {
+        const appComponent = findByTestAttr(wrapper, "component-app");
 
-    expect(appComponent.length).toBe(1);
-  });
-  it('should renders inc btn', () => {});
-  it('should renders counter display', () => {});
-  it('counter should display should starts from 0', () => {});
-  it('should inc the counter', () => {});
+        expect(appComponent.length).toBe(1);
+    });
+    it('should renders inc btn', () => {
+        const button = findByTestAttr(wrapper, "increment-button");
+
+        expect(button.length).toBe(1);
+    });
+    it('should renders counter display', () => {
+        const counterDisplay = findByTestAttr(wrapper, "counter-display");
+
+        expect(counterDisplay.length).toBe(1);
+    });
+    it('counter should display should starts from 0', () => {
+        const count = findByTestAttr(wrapper, 'count');
+
+        expect(count.text()).toBe('0');
+    });
+    it('should inc the counter', () => {
+        const button = findByTestAttr(wrapper, "increment-button");
+        button.simulate('click');
+        const count = findByTestAttr(wrapper, 'count');
+        expect(count.text()).toBe('1')
+    });
+
+    it('should dec counter', () => {
+        /*
+        * simulate and props().OnClick() are close to each other
+        * in best practies it is recomended to use props().onClick()
+        * */
+
+        findByTestAttr(wrapper, 'increment-button').simulate('click');
+        findByTestAttr(wrapper, 'increment-button').simulate('click');
+
+        expect(findByTestAttr(wrapper, "count").text()).toBe('2');
+
+        findByTestAttr(wrapper, 'decrement-button').props().onClick();
+        findByTestAttr(wrapper, 'decrement-button').props().onClick();
+        expect(findByTestAttr(wrapper, "count").text()).toBe('0');
+    });
+
+    it('should display error message if user try to dec counter when it is already 0', () => {
+
+        expect(findByTestAttr(wrapper, 'count').text()).toBe('0');
+        expect(findByTestAttr(wrapper, 'error-message').length).toBe(0);
+
+        findByTestAttr(wrapper, 'decrement-button').props().onClick();
+        expect(findByTestAttr(wrapper, 'error-message').length).toBe(1);
+    });
+
+    it('should hide error message if user try to inc and error message is shown', () => {
+
+        expect(findByTestAttr(wrapper, 'count').text()).toBe('0');
+
+        findByTestAttr(wrapper, 'decrement-button').props().onClick();
+        expect(findByTestAttr(wrapper, 'error-message').length).toBe(1);
+
+        findByTestAttr(wrapper, 'increment-button').props().onClick();
+        expect(findByTestAttr(wrapper, 'error-message').length).toBe(0);
+        expect(findByTestAttr(wrapper, 'count').text()).toBe('1');
+
+    });
 });
