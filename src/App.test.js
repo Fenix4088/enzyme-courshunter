b20fbee614bd3ff942bbe58d31440b260d5dc088
Yehor Pliasov <fenix4088@gmail.com>
2021-10-09 20:41:00 +0300

    create global mock for get secret word

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,19 @@
 import App from './App';
 import { findByTestAttr, setUp } from './utils/utilsForTesting';
+import { getSecretWord as mockGetSecretWord } from './actions';
+
+//activate global mock to make sure getSecretWord dosen't make network call
+jest.mock('./actions');
 
 
 describe('app', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = setUp(App)
-  })
+    wrapper = setUp(App);
+  });
 
   it('renders without error', () => {
     const app = findByTestAttr(wrapper, 'component-app');
     expect(app).toHaveLength(1);
   });
-})
+});
