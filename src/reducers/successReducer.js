427c414c967ceecf8ffa6721a39138f0634edee5
Yehor Pliasov <fenix4088@gmail.com>
2021-10-10 17:56:36 +0300

    redux: configuration added

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,14 @@
+const initialState = []
+
+/**
+ *
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - actions to be reducer
+ * @returns {object} - new state
+ */
+export const successReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
