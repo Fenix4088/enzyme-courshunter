c47e1a618ee60fb4ab6e1f7613aa6fd49f9beb26
Yehor Pliasov <fenix4088@gmail.com>
2021-10-17 11:52:58 +0300

    context: modify getStringByLanguage tests

--- src/helpers/strings.test.js
+++ src/helpers/strings.test.js
@@ -3,6 +3,8 @@ import stringModule from './strings';
 const { getStringByLanguage } = stringModule;
 
 describe('getStringByLanguage', () => {
+  const mockWarn = jest.fn();
+  let originalWarn;
 
   const strings = {
     en: { submit: 'submit' },
@@ -11,24 +13,37 @@ describe('getStringByLanguage', () => {
   };
   const SUBMIT = 'submit';
 
+  beforeEach(() => {
+    originalWarn = console.warn;
+    console.warn = mockWarn;
+  });
+
+  afterEach(() => {
+    console.warn = originalWarn;
+  })
+
   it('should returns correct submit for english', () => {
     const string = getStringByLanguage('en', SUBMIT, strings);
 
     expect(string).toBe('submit');
+    expect(mockWarn).not.toHaveBeenCalled();
   });
   it('should returns correct submit for emoji', () => {
     const string = getStringByLanguage('emoji', SUBMIT, strings);
 
     expect(string).toBe('🚀');
+    expect(mockWarn).not.toHaveBeenCalled();
   });
   it('should returns english submit string when language does not exist', () => {
     const string = getStringByLanguage('notALanguage', SUBMIT, strings);
 
     expect(string).toBe('submit');
+    expect(mockWarn).toHaveBeenCalledTimes(1);
   });
   it('should returns english submit string when submit key does not exist for language', () => {
     const string = getStringByLanguage('mermish', SUBMIT, strings);
 
     expect(string).toBe('submit');
+    expect(mockWarn).toHaveBeenCalledTimes(1);
   });
 });
